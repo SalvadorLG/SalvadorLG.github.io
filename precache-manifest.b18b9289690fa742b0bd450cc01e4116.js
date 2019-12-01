8bc65ed16b7c6abd5c31934af062e20bed13bca0
SalvadorLG <153192@ids.upchiapas.edu.mx>
2019-11-30 23:13:55 -0600

    deploy

---
+++ precache-manifest.b18b9289690fa742b0bd450cc01e4116.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e484fce5701f49e952fe",
+    "url": "/css/app.b130a053.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5ad79ac47888ea8f9cad5baa74ff0ea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b597e532fbd6a9850fc2",
+    "url": "/js/about.a347de33.js"
+  },
+  {
+    "revision": "e484fce5701f49e952fe",
+    "url": "/js/app.52a39e1f.js"
+  },
+  {
+    "revision": "87799f30b1e551a41c75",
+    "url": "/js/chunk-vendors.9e69d06a.js"
+  },
+  {
+    "revision": "a40d8f4f5a18c86789f4281721ed4327",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
